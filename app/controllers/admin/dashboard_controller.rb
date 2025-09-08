407f6a3e1156e1dc94e4b0b82bbad85a5f30a878
Jonathan Ferreira <jonathan.vferreira1@gmail.com>
2025-09-08 19:36:44 -0300

    feat(*): add devise gem

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   layout 'admin'
 
+  before_action :authenticate_user!
+
   def index
     @competitions_count = Competition.count
     @recent_competitions = Competition.order(created_at: :desc).limit(5)
